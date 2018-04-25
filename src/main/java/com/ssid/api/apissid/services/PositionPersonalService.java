474b33916395e6761be4776c1d0145519a2c4883
Franz Lopez <franzelunico@gmail.com>
2018-04-25 12:53:30 -0400

    agregando servicios para PositionPersonal

---
+++ src/main/java/com/ssid/api/apissid/services/PositionPersonalService.java
@@ -0,0 +1,9 @@
+package com.ssid.api.apissid.services;
+
+import com.ssid.api.apissid.domain.PositionPersonal;
+
+import java.util.List;
+
+public interface PositionPersonalService {
+    List<PositionPersonal> getAllPositionPersonal();
+}
