fbb6142ab766441f612badf16460bb5d2ff70832
Franz Lopez <franzelunico@gmail.com>
2018-04-25 12:44:09 -0400

    change PositionDetailRepository -> PositionPersonalRepository

--- src/main/java/com/ssid/api/apissid/repositories/PositionPersonalRepository.java
+++ src/main/java/com/ssid/api/apissid/repositories/PositionPersonalRepository.java
@@ -3,5 +3,5 @@ package com.ssid.api.apissid.repositories;
 import com.ssid.api.apissid.domain.PositionPersonal;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PositionDetailRepository extends JpaRepository<PositionPersonal, Long> {
+public interface PositionPersonalRepository extends JpaRepository<PositionPersonal, Long> {
 }
