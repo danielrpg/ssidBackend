2e621f76f3f305ffa0dcf45748484d695ebcdca1
J David Piérola A <jdpapiero@gmail.com>
2018-04-25 17:30:24 -0400

    Domain Personal and position Merge and fix

--- src/main/java/com/ssid/api/apissid/domain/Position.java
+++ src/main/java/com/ssid/api/apissid/domain/Position.java
@@ -11,23 +11,23 @@ import java.util.Date;
 @Entity
 @Table(name = "position")
 public class Position extends ModelBase implements Serializable {
-    private static final long serialVersionUID=1L;
+    private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "position_id")
     private Long id;
 
-    @Column(name= "position_name", length = 300, unique=true)
+    @Column(name = "position_name", length = 300, unique = true)
     private String name;
 
-    @Column(name= "position_description", length = 800)
+    @Column(name = "position_description", length = 800)
     private String description;
 
     @Column(name = "position_function")
     private String function;
 
-    @Column(name= "position_level")
+    @Column(name = "position_level")
     private Integer level;
 
     @Column(name = "position_init_date")
@@ -54,14 +54,6 @@ public class Position extends ModelBase implements Serializable {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 
     public String getFunction() {
