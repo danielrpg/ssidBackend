2f59f98a3b25bdbfc8a61933aa967575d53ca507
Marcos Bustos <marbusjim@gmail.com>
2018-04-25 01:38:38 -0400

    Creacion de servicios y controles

---
+++ src/main/java/com/ssid/api/apissid/repositories/RiskIpercRepository.java
@@ -0,0 +1,7 @@
+package com.ssid.api.apissid.repositories;
+
+import com.ssid.api.apissid.domain.RiskIperc;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RiskIpercRepository extends JpaRepository<RiskIperc, Long> {
+}
