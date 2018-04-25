e032fe25555f1342e01cf89fac8be265528845cc
Franz Lopez <franzelunico@gmail.com>
2018-04-25 12:30:31 -0400

    agregando PositionService y PositionServiceImpl

---
+++ src/main/java/com/ssid/api/apissid/services/PositionService.java
@@ -0,0 +1,9 @@
+package com.ssid.api.apissid.services;
+
+import com.ssid.api.apissid.domain.Position;
+
+import java.util.List;
+
+public interface PositionService {
+    List<Position>  getAllPositions();
+}
