40eb9cf4aa32578a4096404c9b9bc518fea3d86e
borisytu2014 <borisytu2014@gmail.com>
2018-04-26 09:12:36 -0400

    user repository

--- src/main/java/com/ssid/api/apissid/repositories/UserRepository.java
+++ src/main/java/com/ssid/api/apissid/repositories/UserRepository.java
@@ -4,8 +4,8 @@ package com.ssid.api.apissid.repositories;
  * @author Borisytu
  */
 
-import com.ssid.api.apissid.domain.User;
+import com.ssid.api.apissid.domain.UserSystem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User , Long> {
+public interface UserRepository extends JpaRepository<UserSystem, Long> {
 }
