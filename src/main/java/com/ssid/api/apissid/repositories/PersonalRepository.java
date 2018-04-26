9bb8b7db53f7be38c34b8c6f8c7820301a1abab2
J David Piérola A <jdpapiero@gmail.com>
2018-04-26 08:51:44 -0400

    Cambio a Response de controller CRUD Area y mejoras

--- src/main/java/com/ssid/api/apissid/repositories/PersonalRepository.java
+++ src/main/java/com/ssid/api/apissid/repositories/PersonalRepository.java
@@ -2,13 +2,15 @@ package com.ssid.api.apissid.repositories;
 
 import com.ssid.api.apissid.domain.Personal;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Created by Jesus David Piérola Alvarado
+ */
 public interface PersonalRepository extends JpaRepository<Personal, Long> {
+    @Query("SELECT p FROM personal p WHERE p.personal_name like ?1")
     Optional<List<Personal>> findByName(String name);
-
-    @Override
-    List<Personal> findAll();
 }
