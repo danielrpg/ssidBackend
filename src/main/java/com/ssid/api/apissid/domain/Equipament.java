9dc8a65af90289dfd5c5953a325f89cea5a68826
danielrpg <daniel.fernandezgp@gmail.com>
2018-04-29 12:20:16 -0400

    Cambios en la JWT token ahora se puede conectar desde la base de datos

--- src/main/java/com/ssid/api/apissid/domain/Equipament.java
+++ src/main/java/com/ssid/api/apissid/domain/Equipament.java
@@ -17,7 +17,7 @@ public class Equipament extends ModelBase implements Serializable {
     private static final long serialVersionUID=1L;
 
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     @Column(name = "equipament_id")
     private Long id;
 
